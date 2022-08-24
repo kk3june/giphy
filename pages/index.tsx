5a54359ca59b7d6aae732a5c982866e9052d41ec
3jun <kk3june@gmail.com>
2022-08-24 17:39:09 +0900

    delete. 불필요한 파일 및 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import styles from '../src/styles/Home.module.css';
 
 function Home() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-      </main>
-    </div>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
