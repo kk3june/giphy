46c590f9fb1052ed25522a3822ab22238f22784d
3jun <kk3june@gmail.com>
2022-08-28 23:27:15 +0900

    feat. trending carousel 레이아웃 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Carousel from '../src/components/modules/Carousel';
+import Header from '../src/components/atoms/Header';
+import { css } from '@emotion/react';
+import Head from 'next/head';
 
 function Home() {
-  return <div>Home</div>;
+  const [trendingList, setTrendingList] = useState<any[]>();
+
+  const getTrending = () => {
+    axios({
+      method: 'get',
+      url: 'https://api.giphy.com/v1/gifs/trending?api_key=RqsM2YqZIBX1z7mAkpHMkyhGQpaCvEiG&limit=25&rating=g',
+    }).then(res => {
+      setTrendingList(res.data.data);
+    });
+  };
+
+  useEffect(() => {
+    getTrending();
+    console.log('rendering');
+  }, []);
+
+  return (
+    <>
+      <Header name="trending" />
+      <div
+        css={css`
+          display: flex;
+          width: 100%;
+          height: 140px;
+          overflow: hidden;
+        `}
+      >
+        {trendingList &&
+          trendingList.map((trending, idx) => (
+            <Carousel key={idx} data={trending.images.original.url} />
+          ))}
+      </div>
+    </>
+  );
 }
 
 export default Home;
---
+++ src/components/modules/Carousel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Carosel = ({ data }: any) => {
+  return (
+    <>
+      <img src={data} />
+    </>
+  );
+};
+
+export default Carosel;
