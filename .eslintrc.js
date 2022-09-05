9347b73a1f859492d921274c5e7997b98c4a0f40
3jun <kk3june@gmail.com>
2022-09-05 16:54:55 +0900

    fix. eslint, prettierrc, tsconfig 설정 추가

---
+++ .eslintrc.js
@@ -0,0 +1,87 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', '@typescript-eslint', 'react-hooks'],
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    'prettier/prettier': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.stories.tsx',
+          '**/*.@(spec|test).@(js|ts)?(x)',
+          '**/testUtils.tsx',
+          '**/jest.setup.ts',
+          '**/.storybook/*.@(js|ts)?(x)',
+          '**/webpack.*.js',
+          '**/script/*.js',
+          '**/mocks/**/*.@(js|ts)?(x)',
+        ],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.tsx', '.jsx', 'spec.js'] },
+    ],
+    'react/function-component-definition': 'off',
+    'import/no-duplicates': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/no-unresolved': [2, { ignore: ['\\.img$'] }],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '[@]common/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      { controlComponents: ['input', 'select'] },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
