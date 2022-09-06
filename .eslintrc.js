4931d691b8543b7f512076ab8d39b27518c6f715
3jun <kk3june@gmail.com>
2022-09-06 14:47:59 +0900

    fix. path 설정에 의한 eslint 에러 디버깅(eslint-import-resolver-typescript lib 설치 및 .eslintrc 파일 수정), eslint css 관련 에러 디버깅

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-props-no-spreading': 'off',
+    "react/no-unknown-property": ["error", { "ignore": ["css"] }],
     'react/jsx-filename-extension': [
       'error',
       { extensions: ['.tsx', '.jsx', 'spec.js'] },
@@ -78,10 +79,17 @@ module.exports = {
     ],
   },
   settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
+    // 'import/resolver': {
+    //   node: {
+    //     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    //   },
+    // },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
     },
+    "import/resolver": {
+      "typescript": {}
+    }
   },
 };
+
