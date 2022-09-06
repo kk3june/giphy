55b20bc39f15c9744fa6cbb5e10ecbc100815c68
3jun <kk3june@gmail.com>
2022-09-06 23:45:41 +0900

    fix. sidebar 컴포넌트 source 탭 없는 경우 예외처리

--- src/components/modules/SidebarBox/SidebarBox.tsx
+++ src/components/modules/SidebarBox/SidebarBox.tsx
@@ -3,9 +3,19 @@ import { css } from '@emotion/react';
 import UploadIcon from '../../atoms/Svgs/UploadIcon';
 import UserInfo from '../../atoms/UserInfo/UserInfo';
 
-import { StyledSpan, SnsIcon, SourceLink, SourceLinkUrl, LearnMoreGif, StyledTextBox, SideGif, SideGifIcon } from './SidebarBox.styled';
+import {
+  StyledSpan,
+  SnsIcon,
+  SourceLink,
+  SourceLinkUrl,
+  LearnMoreGif,
+  StyledTextBox,
+  SideGif,
+  SideGifIcon,
+} from './SidebarBox.styled';
 
 const SidebarBox = ({ data }: any) => {
+  console.log(data);
   const userInfoProps = {
     type: 'sidebar',
     name: data?.[0].user?.name,
@@ -21,9 +31,11 @@ const SidebarBox = ({ data }: any) => {
       `}
     >
       {data?.[0].user ? (
-        <div css={css`
-        width: inherit;
-        `}>
+        <div
+          css={css`
+            width: inherit;
+          `}
+        >
           <UserInfo {...userInfoProps} />
           <StyledSpan>{data?.[0].user?.description}</StyledSpan>
           <StyledSpan>Follow on:</StyledSpan>
@@ -37,44 +49,45 @@ const SidebarBox = ({ data }: any) => {
           <SnsIcon href={data?.[0].user?.twitter_url}>
             <img src="/images/snsIcon/twitter.png" alt="link to twitter" />
           </SnsIcon>
-          <StyledSpan>Source</StyledSpan>
-          <SourceLink href={data?.[0].user?.website_url} type="bold">
-            <img
-              css={css`
-                width: 16px;
-                margin-right: 5px;
-                filter: brightness(0) invert(1);
-              `}
-              src="/images/snsIcon/external_link.png"
-              alt="external link"
-            />
-            <SourceLinkUrl>{data?.[0].user?.website_display_url}</SourceLinkUrl>
-          </SourceLink>
+
+          {data?.[0].user?.website_url && (
+            <>
+              <StyledSpan>Source</StyledSpan>
+              <SourceLink href={data?.[0].user?.website_url} type="bold">
+                <img
+                  css={css`
+                    width: 16px;
+                    margin-right: 5px;
+                    filter: brightness(0) invert(1);
+                  `}
+                  src="/images/snsIcon/external_link.png"
+                  alt="external link"
+                />
+                <SourceLinkUrl>{data?.[0].user?.website_display_url}</SourceLinkUrl>
+              </SourceLink>
+            </>
+          )}
         </div>
       ) : (
         <SideGif>
-        <StyledSpan
-          css={css`
-            font-weight: bold;
-          `}
-        >
-          Create A Gif
-        </StyledSpan>
-        <SideGifIcon>
-          <UploadIcon />
-          Gif Maker
-        </SideGifIcon>
-        <StyledSpan>
-          Create animated GIFs from video files and YouTube links
-        </StyledSpan>
+          <StyledSpan
+            css={css`
+              font-weight: bold;
+            `}
+          >
+            Create A Gif
+          </StyledSpan>
+          <SideGifIcon>
+            <UploadIcon />
+            Gif Maker
+          </SideGifIcon>
+          <StyledSpan>Create animated GIFs from video files and YouTube links</StyledSpan>
 
-        <StyledTextBox>
-          <a href="https://giphy.com/backdrop">Create Your own</a>
-        </StyledTextBox>
-      </SideGif>
+          <StyledTextBox>
+            <a href="https://giphy.com/backdrop">Create Your own</a>
+          </StyledTextBox>
+        </SideGif>
       )}
-
-      
 
       <div
         css={css`
