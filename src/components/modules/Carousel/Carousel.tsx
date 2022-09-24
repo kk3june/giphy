27d40d3e0d356aeb74236b7d06e6afc7df2b133c
3jun <kk3june@gmail.com>
2022-09-24 14:35:15 +0900

    fix. swiper 라이브러리 사용하여 구현한 캐러셀 기능 에러 디버깅(width auto 관련 문제)

--- src/components/modules/Carousel/Carousel.tsx
+++ src/components/modules/Carousel/Carousel.tsx
@@ -7,8 +7,8 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
+import { StyledWrapper } from 'components/modules/Carousel/Carousel.styled';
 import CardLayer from 'layer/CardLayer';
-import { TRENDING, ARTISTS } from 'src/constants';
 
 interface CarouselLayerProps {
   type: string | undefined;
@@ -16,34 +16,24 @@ interface CarouselLayerProps {
 }
 
 const Carousel = ({ data, type }: CarouselLayerProps) => {
-  // Swiper style
-  const SwiperStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    overflow: 'hidden',
-  };
-  const SlideStyle = {
-    width: 'auto',
-    height: 'auto',
-  };
-
   return (
-    <Swiper
-      style={SwiperStyle}
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={0}
-      slidesPerView="auto"
-      navigation
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {data &&
-        data.map((item: any) => (
-          <SwiperSlide key={item.id} style={SlideStyle}>
-            <CardLayer data={item} type={type} />
-          </SwiperSlide>
-        ))}
-    </Swiper>
+    <StyledWrapper>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {data &&
+          data.map((item: any) => (
+            <SwiperSlide key={item.id}>
+              <CardLayer data={item} type={type} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </StyledWrapper>
   );
 };
 
