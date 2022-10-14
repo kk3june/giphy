9c551556ffd2d76e8bba65ddef44b09943aa1ef7
3jun <kk3june@gmail.com>
2022-10-15 01:21:22 +0900

    styled. trending, atrists 컴포넌트 hover animation 구현

--- src/components/atoms/Card/Card.styled.tsx
+++ src/components/atoms/Card/Card.styled.tsx
@@ -23,10 +23,23 @@ const HEIGHT_TYPE = {
 };
 
 export const StyledCard = styled.div<StyledCardProps>`
+  position: relative;
   width: ${({ type }) => WIDTH_TYPE[type as keyof typeof WIDTH_TYPE]};
   height: ${({ type }) => HEIGHT_TYPE[type as keyof typeof HEIGHT_TYPE]};
   margin-right: 0.2rem;
   margin-bottom: 0.2rem;
+  &:hover {
+    .trending_hover > * {
+      visibility: visible;
+    }
+    .artists_hover > * {
+      visibility: visible;
+    }
+    .image {
+      transform: translate(-44px, 50px);
+      transition: transform 0.3s;
+    }
+  }
 `;
 
 export const StyledImg = styled.img<StyledCardProps>`
@@ -36,4 +49,57 @@ export const StyledImg = styled.img<StyledCardProps>`
   height: 100%;
   width: ${({ type }) => WIDTH_TYPE[type as keyof typeof WIDTH_TYPE]};
   object-fit: cover;
+`;
+
+export const TrendingHoverSvgs = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  top: 5px;
+  right: 8px;
+  width: 3rem;
+  visibility: hidden;
+`;
+
+export const TrendingHoverBadge = styled.a`
+  position: absolute;
+  cursor: pointer;
+  bottom: 5px;
+  left: 8px;
+  visibility: hidden;
+`;
+
+export const ArtistHoverWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  opacity: 0.33;
+  visibility: hidden;
+`;
+export const ArtistsHoverImg = styled.div`
+  position: absolute;
+  top: -50px;
+  right: -50px;
+  visibility: hidden;
+`;
+
+export const ArtistsBadge = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  bottom: 5px;
+  left: 8px;
+  span {
+    display: block;
+    padding-left: 10px;
+    font-size: 1.125rem;
+    line-height: 1.3rem;
+    font-weight: bold;
+  }
+  span:nth-child(2) {
+    color: rgb(0, 255, 153);
+    font-size: 0.875rem;
+  }
 `;
--- src/components/atoms/Card/Card.tsx
+++ src/components/atoms/Card/Card.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
-import { GIF, LOGIN_PAGE } from 'src/constants';
+import { ARTISTS, GIF, LOGIN_PAGE, TRENDING } from 'src/constants';
 
-import { StyledCard, StyledImg } from './Card.styled';
+import LikeSvg from '../Svgs/LikeSvg';
+import LinkSvg from '../Svgs/LinkSvg';
+
+import {
+  StyledCard,
+  StyledImg,
+  TrendingHoverBadge,
+  TrendingHoverSvgs,
+  ArtistsHoverImg,
+  ArtistsBadge,
+  ArtistHoverWrapper,
+} from './Card.styled';
 
 const Card = ({ data, type, isLoading }: any) => {
+  if (type === ARTISTS) {
+    console.log(type, data);
+  }
   return isLoading ? (
     <div
       css={{
@@ -27,6 +41,36 @@ const Card = ({ data, type, isLoading }: any) => {
         </a>
       )}
       {type === LOGIN_PAGE && <StyledImg src={data?.images?.original.url} type={type} />}
+
+      {type === TRENDING && (
+        <div className="trending_hover">
+          <TrendingHoverSvgs>
+            <LikeSvg />
+            <LinkSvg />
+          </TrendingHoverSvgs>
+          <TrendingHoverBadge href={`/gifs/${data?.id}`}>
+            {data.user && <img src={data.user.avatar_url} width={35} height={35} alt="user_badge" />}
+          </TrendingHoverBadge>
+        </div>
+      )}
+
+      {type === ARTISTS && (
+        <>
+          <a href={`/gifs/${data?.id}`} className="artists_hover">
+            <ArtistHoverWrapper />
+            <ArtistsHoverImg className="image">
+              <img src="/images/hover_page_image.png" width={50} height={50} alt="artists hover" />
+            </ArtistsHoverImg>
+          </a>
+          <ArtistsBadge>
+            {data.user && <img src={data.user.avatar_url} width={40} height={40} alt="user_badge" />}
+            <div>
+              <span>{data.username}</span>
+              <span>{data.username && '• Available for work'}</span>
+            </div>
+          </ArtistsBadge>
+        </>
+      )}
     </StyledCard>
   );
 };
